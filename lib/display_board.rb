550dd6c1a184040d8f4a960d3e45124ed2cdbce1
Alex Wilson-Campbell <alex@liveworkplay365.com>
2019-05-12 10:35:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board  # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-  puts "    |   |   "
+  puts "   |   |   "
   puts "-----------"
-  puts "    |   |   "
+  puts "     |    |    "
   puts "-----------"
-  puts "    |   |   "
+  puts "     |    |    "
 end
